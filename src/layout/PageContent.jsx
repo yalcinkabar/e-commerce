1bc5b595840d038df6a9c9ecb429a7db5fecd162
Yalcin KABAR <yalcinkabar@gmail.com>
2026-06-09 22:52:40 +0300

    feat: add contact team and about pages

--- src/layout/PageContent.jsx
+++ src/layout/PageContent.jsx
@@ -2,6 +2,9 @@ import { Switch, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import ShopPage from "../pages/ShopPage";
 import ProductDetailPage from "../pages/ProductDetailPage";
+import ContactPage from "../pages/ContactPage";
+import TeamPage from "../pages/TeamPage";
+import AboutPage from "../pages/AboutPage";
 
 function PageContent() {
   return (
@@ -10,6 +13,9 @@ function PageContent() {
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
         <Route exact path="/product/:id" component={ProductDetailPage} />
+        <Route exact path="/contact" component={ContactPage} />
+        <Route exact path="/team" component={TeamPage} />
+        <Route exact path="/about" component={AboutPage} />
       </Switch>
     </main>
   );
